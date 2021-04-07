7cde4bc606a8c1a581afaea5637f6b1a03ec9bdc
Khanh <khanh23599@gmail.com>
2021-04-07 14:29:17 +0700

    up

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.scss';
+
+Footer.propTypes = {
+    
+};
+
+function Footer(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+Header.propTypes = {};
+
+function Header(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Header;
